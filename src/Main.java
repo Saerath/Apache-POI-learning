2411085799233bf033340b2b72c09bb35223e9c4
Saerath <saerath13@gmail.com>
2019-02-26 13:05:43 +0300

    Test creating excel table

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
-import org.apache.poi.hssf.usermodel.*;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-import org.apache.poi.ss.usermodel.*;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.ss.usermodel.*;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
 		// TODO Auto-generated method stub
 		Workbook book = new HSSFWorkbook();
-		POIFSFileSystem 
+		Sheet sheet = book.createSheet("test");
+		Row row = sheet.createRow(0);
+		Cell cell = row.createCell(0);
+		cell.setCellValue(0);
+		String test = "test.xls";
+		book.write(new FileOutputStream(test));
+		book.close();
 	}
 
 }
